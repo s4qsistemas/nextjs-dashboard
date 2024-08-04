39e2ddcaa831a45220a1380bd86473a9501edcbe
Diego Ignacio Alvial Arrepol <sigm4q@gmail.com>
2024-08-04 15:53:57 -0400

    commit inicial

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Montserrat, Lusitana, Inter } from 'next/font/google';
+
+export const montserrat = Montserrat({ subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: ['400'], subsets: ['latin'] });
+export const inter = Inter({ subsets: ['latin'] });
